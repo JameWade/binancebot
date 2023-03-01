29e250fd798c40425fb85a82a27b03af0c4f774e
79022872 <790228724@qq.com>
2023-03-01 20:36:14 +0800

    更新candlelist

--- src/main/java/com/example/demo/bean/CandleEntryList.java
+++ src/main/java/com/example/demo/bean/CandleEntryList.java
@@ -6,23 +6,13 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @ToString
-public class CandleEntry {
-    private String startTime;                                // k线开盘时间
-    private BigDecimal openingPrice;                              // 开盘价
-    private BigDecimal highestPrice;                              // 最高价
-    private BigDecimal lowestPrice;                               // 最低价
-    private BigDecimal endPrice;                                 // 收盘价(当前K线未结束的即为最新价)
-    private BigDecimal transactionVolume;                     // 成交量
-    private String endTime;                                            // k线收盘时间
-    private BigDecimal turnover;                                              // 成交额
-    private int transactionNumber;                                         // 成交笔数
-    private BigDecimal tradingVolume;                                    // 主动买入成交量
-    private BigDecimal tradingTurnover;                              // 主动买入成交额
-    private BigDecimal other;                                      // 请忽略该参数
-
+public class CandleEntryList {
+    private String symbol;//交易对昵称
+    private List<CandleEntry> candleEntries;
 }
--- src/main/java/com/example/demo/config/SymbolConfig.java
+++ src/main/java/com/example/demo/config/SymbolConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.config;
 
+import com.example.demo.bean.CandleEntry;
+import com.example.demo.bean.CandleEntryList;
+
 import java.util.List;
 
 public class SymbolConfig {
@@ -7,6 +10,9 @@ public class SymbolConfig {
 
     //存放币安所有市场代码
     public static List<String> martket_tickers;
+
+    //存放所有交易对的最近1000条收盘价格
+    public static List<CandleEntryList> candleEntryLists;
 
     //币安请求路由
     public static String baseUrl = "https://api3.binance.com/api/v3";
