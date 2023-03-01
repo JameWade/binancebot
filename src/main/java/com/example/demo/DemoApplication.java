f75cb2c645a2006786be2725535d6c68b75ab292
79022872 <790228724@qq.com>
2023-03-01 18:25:01 +0800

    Merge branch 'master' of https://github.com/JameWade/binancebot

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.config.InitConfig;
+import com.example.demo.config.SymbolConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableScheduling
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(DemoApplication.class, args);
+
+        //初始化交易对
+        InitConfig.init();
+        //打印已存在的交易对
+        System.out.println(SymbolConfig.martket_tickers);
+        System.out.println(SymbolConfig.martket_tickers.size());
     }
 
 }
--- src/main/java/com/example/demo/config/InitConfig.java
+++ src/main/java/com/example/demo/config/InitConfig.java
@@ -1,31 +1,70 @@
 package com.example.demo.config;
 
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.common.okhttp.OkHttpUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * 初始化交易对
  */
+@Slf4j
 public class InitConfig {
 
     /**
      * 初始化 交易对数据
+     *
      * @throws Exception
      */
     public static void init() throws Exception {
+        List<String> martket_tickers = new ArrayList<>();
         //获取请求地址
         String priceUrl = SymbolConfig.baseUrl + SymbolConfig.pricePath;
-         OkHttpUtils.builder()
+        String pricesStr = OkHttpUtils.builder()
                 .url(priceUrl)
                 .get()
                 .sync();
+        JSONArray parse = (JSONArray) JSONArray.parse(pricesStr);
+        parse.forEach(x -> {
+            JSONObject data = (JSONObject) x;
+            //不等于 -1 说明匹配到了关键字 进行存储
+            String symbol = data.getString("symbol");
+            if (symbol.substring(symbol.length() - 4).indexOf("BUSD") != -1) {
+                martket_tickers.add(symbol);
+            }
+        });
+
+        if (martket_tickers == null || martket_tickers.isEmpty()) {
+            log.error("初始化交易对失败");
+            throw new Exception("初始化交易对失败");
+        }
+        SymbolConfig.martket_tickers = martket_tickers;
     }
 
     public static void main(String[] args) {
+        List<String> martket_tickers = new ArrayList<>();
         String async = OkHttpUtils.builder()
                 .url("https://api.binance.com/api/v3/ticker/price")
                 .get()
                 .sync();
-        System.out.println(async);
+
+        JSONArray parse = (JSONArray) JSONArray.parse(async);
+        for (Object obj : parse) {
+            JSONObject data = (JSONObject) obj;
+
+            //不等于 -1 说明匹配到了关键字 进行存储
+            String symbol = data.getString("symbol");
+            //
+            if (symbol.substring(symbol.length() - 4).indexOf("BUSD") != -1) {
+                martket_tickers.add(symbol);
+            }
+        }
+        System.out.println(martket_tickers);
     }
 }
